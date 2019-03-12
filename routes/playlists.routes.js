b995909ece8c26c43feb28e07d464abd2ac70116
Raphael <rrhassan@umich.edu>
2019-03-12 11:53:51 -0400

    added some postgres functionality

--- routes/playlists.routes.js
+++ routes/playlists.routes.js
@@ -10,9 +10,13 @@ playlists.get("/playlists", function(req,res) {
 });
 
 playlists.post("/playlists", function(req, res){
-  pool.query("insert into playlists (name, category, tags, data) values ($1::text, $2::text, $3::array, $4::json)", [req.body.name, req.body.category, req.body.tags, req.body.data]).then(function(){
+  pool.query("insert into playlists (name, category, tags, data) values ($1::text, $2::text, $3::text[], $4::jsonb)", [req.body.name, req.body.category, req.body.tags, req.body.data]).then(function(){
     pool.query("select * from playlists").then(function(result) {
       res.send(result.rows);
+      console.log(req.body);
+      console.log(result.rows);
+      
+      
     });
   });
 });
