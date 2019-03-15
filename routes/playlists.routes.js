0007123d89554bbd47d9676f66d6b77fd0f297b5
Kathy <kpnelson847@gmail.com>
2019-03-15 10:26:13 -0400

    added the delete button to they settings page, added order by id in the pool queries in the playlists.jpg, and added a bottom margin to home_container

--- routes/playlists.routes.js
+++ routes/playlists.routes.js
@@ -4,14 +4,14 @@ const playlists = express.Router();
 const pool = require("./connection");
 
 playlists.get("/playlists", function(req,res) {
-  pool.query("select * from playlists").then(function(result) {
+  pool.query("select * from playlists order by id").then(function(result) {
     res.send(result.rows);
   });
 });
 
 playlists.post("/playlists", function(req, res){
   pool.query("insert into playlists (name, category, tags, data) values ($1::text, $2::text, $3::text[], $4::jsonb)", [req.body.name, req.body.category, req.body.tags, req.body.data]).then(function(){
-    pool.query("select * from playlists").then(function(result) {
+    pool.query("select * from playlists order by id").then(function(result) {
       res.send(result.rows);
     });
   });
@@ -19,7 +19,7 @@ playlists.post("/playlists", function(req, res){
 
 playlists.delete("/playlists/:id", function(req, res) {
   pool.query("delete from playlists where id=$1::int", [req.params.id]).then(function() {
-    pool.query("select * from playlists").then(function(result) {
+    pool.query("select * from playlists order by id").then(function(result) {
       res.send(result.rows);   
     })
   })
@@ -28,7 +28,7 @@ playlists.delete("/playlists/:id", function(req, res) {
 playlists.put("/playlists/:id", function(req, res) {
   pool.query("update playlists set tags=$1::text[], data=$2::jsonb where id=$3::int", [[], {}, req.params.id]).then(function() {
     pool.query("update playlists set name=$1::text, category=$2::text, tags=$3::text[], data=$4::jsonb where id=$5::int", [req.body.name, req.body.category, req.body.tags, req.body.data, req.params.id]).then(function() {
-      pool.query("select * from playlists").then(function(result) {
+      pool.query("select * from playlists order by id").then(function(result) {
         res.send(result.rows);
       })
     })
